01bd61f9a4b5b8f7f21e69a886691efca7645fea
Hilina Mulugeta <Hilinamulugeta1557@gmail.com>
2024-12-11 04:04:35 +0300

    save

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import AOS from 'aos';
 import "aos/dist/aos.css";
 import './index.css';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route
 } from 'react-router-dom';
+
 // All pages
 import Home from './pages/Home';
 import Contact from './pages/Contact';
